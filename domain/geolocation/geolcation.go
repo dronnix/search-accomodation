d8a7634a3b511cac6b1c52e30f82e52b7259d404
Andrew Luzin <a.v.luzin@2gis.ru>
2022-07-11 19:36:07 +0400

    Extend domain model

--- domain/geolocation/geolcation.go
+++ domain/geolocation/geolcation.go
@@ -1,7 +1,9 @@
 package geolocation
 
 import (
+	"errors"
 	"fmt"
+	"io"
 	"net"
 	"regexp"
 	"strconv"
@@ -17,9 +19,52 @@ type IPLocation struct {
 	MysteryValue uint64
 }
 
+// ImportIPLocations - imports IP locations with providing statistics.
+func ImportIPLocations(importer IPLocationImporter, storer IPLocationStorer) (ImportStatistics, error) {
+	const batchSize = 1024
+	totalStats := ImportStatistics{}
+	// TODO: Mesure time spent on import.
+	for {
+		ipLocations, stats, err := importer.ImportNextBatch(batchSize)
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				return stats, nil
+			}
+			return stats, fmt.Errorf("failed to import ip locations: %w", err)
+		}
+		if err = storer.StoreIPLocations(ipLocations); err != nil {
+			return stats, fmt.Errorf("failed to store ip locations: %w", err)
+		}
+		totalStats.Add(stats)
+	}
+}
+
+var ErrIPLocationNotFound = errors.New("ip location not found")
+var ErrIPLocationAmbiguous = errors.New("ip location is ambiguous")
+
+// PredictIPLocation figures out IP location from IP address, using given fetcher.
+// Returns ErrIPLocationNotFound if IP location is not found.
+// Returns ErrIPLocationAmbiguous if IP more than one location known for the IP.
+func PredictIPLocation(ip net.IP, fetcher IPLocationFetcher) (IPLocation, error) {
+	return IPLocation{}, errors.New("not implemented")
+}
+
 // IPLocationImporter - interface for importing IP locations from some source.
 type IPLocationImporter interface {
+	// ImportNextBatch returns io.EOF when no more data is available.
 	ImportNextBatch(size int) ([]IPLocation, ImportStatistics, error)
+}
+
+// IPLocationStorer - interface for storing IP locations.
+type IPLocationStorer interface {
+	StoreIPLocations([]IPLocation) error
+}
+
+// IPLocationFetcher - interface for fetching locations by IP address.
+type IPLocationFetcher interface {
+	// FetchLocationsByIP returns all possible locations for given IP address.
+	// If no locations are found, returns empty slice.
+	FetchLocationsByIP(ip net.IP) ([]IPLocation, error)
 }
 
 // ImportStatistics - provides statistics about import process.
