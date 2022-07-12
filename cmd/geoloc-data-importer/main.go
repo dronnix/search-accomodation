af93e18cf718b086f6a8af7e880500669d4a1148
Andrew Luzin <a.v.luzin@2gis.ru>
2022-07-12 13:37:42 +0400

    Add storage stub

--- cmd/geoloc-data-importer/main.go
+++ cmd/geoloc-data-importer/main.go
@@ -1,5 +1,8 @@
 package main
 
-func main() {
+import "github.com/dronnix/search-accomodation/storage"
 
+func main() {
+	s := storage.NewIPLocationStorage(nil)
+	s.MigrateUp(nil, "")
 }
---
+++ storage/IPLocation.go
@@ -0,0 +1,40 @@
+package storage
+
+import (
+	"context"
+	"fmt"
+	"net"
+
+	"github.com/jackc/pgx/v4/pgxpool"
+
+	"github.com/dronnix/search-accomodation/domain/geolocation"
+)
+
+type IPLocationStorage struct {
+	pool *pgxpool.Pool
+}
+
+var _ geolocation.IPLocationFetcher = (*IPLocationStorage)(nil)
+var _ geolocation.IPLocationStorer = (*IPLocationStorage)(nil)
+
+func NewIPLocationStorage(pool *pgxpool.Pool) *IPLocationStorage {
+	return &IPLocationStorage{pool: pool}
+}
+
+func (s *IPLocationStorage) MigrateUp(ctx context.Context, migrationsDir string) error {
+	version, err := Migrate(ctx, s.pool, migrationsDir)
+	if err != nil {
+		return fmt.Errorf("unable to migrate observations to version %d: %w", version, err)
+	}
+	return nil
+}
+
+func (s *IPLocationStorage) FetchLocationsByIP(ip net.IP) ([]geolocation.IPLocation, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s *IPLocationStorage) StoreIPLocations(ctx context.Context, locations []geolocation.IPLocation) error {
+	//TODO implement me
+	panic("implement me")
+}
